b56bff5c3a94fe9a4a682c3942e0f7efad596fbf
Thomas Dubiel <thomas.dubiel@sap.com>
2017-01-23 10:18:55 +0100

    initial commit

---
+++ justdoitxone/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿//
+// App.xaml.cs
+//
+// Created by Thomas Dubiel on 11.01.2017
+// Copyright 2017 Thomas Dubiel (td@crazynexus.de). All rights reserved.
+//
+using Xamarin.Forms;
+
+namespace justdoitxone
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
---
+++ justdoitxone/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿//
+// MainPage.xaml.cs
+//
+// Created by Thomas Dubiel on 11.01.2017
+// Copyright 2017 Thomas Dubiel (td@crazynexus.de). All rights reserved.
+//
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace justdoitxone
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		void OnSaveClicked(object sender, EventArgs e)
+		{
+			lbTest.Text = "Button clicked";
+		}
+
+		void OnSwitchToggled(object sender, EventArgs e)
+		{
+			lbTest.Text = "Switch toggled";
+		}
+	}
+}
